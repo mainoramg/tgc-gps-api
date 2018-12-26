76b9c4fcd52ef208564d14a6606cbee3ac4e0065
mainor <mainor.miranda@bvmedia.cr>
2018-12-25 20:07:57 -0600

    sn added to API URL and support to get more than one event from database

--- ROOT/WEB-INF/classes/com/api1/LatestLocation.java
+++ ROOT/WEB-INF/classes/com/api1/LatestLocation.java
@@ -1,11 +1,14 @@
 package com.api1;
 
-import com.bl.GPSEvent;
+import com.bl.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.util.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.Vector;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,42 +26,76 @@ public class LatestLocation extends HttpServlet
 
       String   uri         =  request.getRequestURI().toString().toLowerCase();
       String   splitURI[]  =  uri.split( "/" );
-      if( splitURI != null && splitURI.length > 4 )
+      if( splitURI != null && splitURI.length > 5 )
       {
-         String   sn =  "";
-                  sn =  splitURI[ 4 ];
+         String   sn    =  "";
+                  sn    =  splitURI[ 5 ];
+         String   limit =  "1";
+         if( splitURI.length > 6 )
+         {
+            limit =  splitURI[ 6 ];
+            if( limit != null && !limit.equals( "" ) )
+            {
+               try
+               {
+                  int totalRecords = Integer.parseInt( limit );
+                  if( totalRecords > 100 )
+                  {
+                     limit =  "100";
+                  }
+               }
+               catch( Exception e )
+               {
+                  e.printStackTrace();
+                  limit =  "1";
+               }
+            }
+            else
+            {
+               limit =  "1";
+            }
+         }
          if( sn != null && !sn.equals( "" ) )
          {
             try
             {
-               long validSn = Long.parseLong( sn );
-               HashMap record = DB.getRecord( "select * from gps_data where serial_number = '" + sn + "' order by stamp desc limit 1" );
-               GPSEvent event = new GPSEvent(
-                                             record.get("id").toString(), 
-                                             record.get("maker").toString(), 
-                                             record.get("serial_number").toString(), 
-                                             record.get("command").toString(), 
-                                             record.get("location_stamp").toString(), 
-                                             record.get("status").toString(), 
-                                             record.get("latitude").toString(), 
-                                             record.get("latitude_cardinal").toString(), 
-                                             record.get("latitude_degree").toString(), 
-                                             record.get("latitude_minutes").toString(), 
-                                             record.get("latitude_seconds").toString(), 
-                                             record.get("longitude").toString(), 
-                                             record.get("longitude_cardinal").toString(), 
-                                             record.get("longitude_degree").toString(), 
-                                             record.get("longitude_minutes").toString(), 
-                                             record.get("longitude_seconds").toString(), 
-                                             record.get("speed").toString(), 
-                                             record.get("direction").toString(), 
-                                             record.get("vehicle_status").toString(), 
-                                             record.get("net_mcc").toString(), 
-                                             record.get("net_mnc").toString(), 
-                                             record.get("net_lac").toString(), 
-                                             record.get("net_cell_id").toString()
-                                          );
-               result = event.toJson();
+               long        validSn  =  Long.parseLong( sn );
+               Vector      records  =  DB.getData( "select * from gps_data where serial_number = '" + sn + "' order by location_stamp desc limit " + limit );
+               int         total    =  records.size();
+               GPSEvents   events   =  new GPSEvents();
+               for( int i = 0; i < total; i++ )
+               {
+                  HashMap record = (HashMap) records.elementAt(i);
+                  GPSEvent event = new GPSEvent(
+                                                record.get("id").toString(), 
+                                                record.get("maker").toString(), 
+                                                record.get("serial_number").toString(), 
+                                                record.get("command").toString(), 
+                                                record.get("location_stamp").toString(), 
+                                                record.get("status").toString(), 
+                                                record.get("latitude").toString(), 
+                                                record.get("latitude_cardinal").toString(), 
+                                                record.get("latitude_degree").toString(), 
+                                                record.get("latitude_minutes").toString(), 
+                                                record.get("latitude_seconds").toString(), 
+                                                record.get("longitude").toString(), 
+                                                record.get("longitude_cardinal").toString(), 
+                                                record.get("longitude_degree").toString(), 
+                                                record.get("longitude_minutes").toString(), 
+                                                record.get("longitude_seconds").toString(), 
+                                                record.get("speed").toString(), 
+                                                record.get("direction").toString(), 
+                                                record.get("vehicle_status").toString(), 
+                                                record.get("net_mcc").toString(), 
+                                                record.get("net_mnc").toString(), 
+                                                record.get("net_lac").toString(), 
+                                                record.get("net_cell_id").toString()
+                                             );
+                  events.addEvent( event );
+               }
+               Gson gson   =  new Gson();
+               result      =  gson.toJson( events );
+               result      =  result.replace( "\\u003d", "=" );
             }
             catch( Exception e )
             {
